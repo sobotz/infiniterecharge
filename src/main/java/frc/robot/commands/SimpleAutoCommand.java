5fee083ec2feaedaaf0412282d2e3a1b56bffd6a
Nicholas Cabrera <ncabrera528@gmail.com>
2020-03-05 16:17:42 -0500

    auto done

--- src/main/java/frc/robot/commands/SimpleAutoCommand.java
+++ src/main/java/frc/robot/commands/SimpleAutoCommand.java
@@ -7,6 +7,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LauncherSubsystem;
@@ -17,14 +18,14 @@ public class SimpleAutoCommand extends CommandBase {
   private final SerializerSubsystem m_serializer;
   private LauncherSubsystem m_launcher;
   private boolean isFinished = false;
-  private int nFramesRun;
-  
+  private Timer timer;
+
   public SimpleAutoCommand(DriveSubsystem drive, SerializerSubsystem serial, LauncherSubsystem launch) {
     this.m_drive = drive;
     this.m_serializer = serial;
     this.m_launcher = launch;
 
-    this.nFramesRun = 0;
+    this.timer = new Timer();
 
     addRequirements(this.m_drive, this.m_launcher, this.m_serializer);
   }
@@ -32,17 +33,37 @@ public class SimpleAutoCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    this.nFramesRun = 0;
+    this.timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
+    if(timer.get() < 5){
+      this.m_launcher.startLauncher();
+      this.m_launcher.startRollers();
+      this.m_serializer.runSerializer();
+    }else if(timer.get() < 7){
+      this.m_launcher.stopLauncher();
+      this.m_launcher.stopRollers();
+      this.m_drive.manualDrive2(-0.5, 0);
+    }else{
+      this.m_drive.manualDrive2(0, 0);
+      this.m_launcher.stopLauncher();
+      this.m_launcher.stopRollers();
+    }
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    this.m_drive.manualDrive2(0, 0);
+    this.m_launcher.stopLauncher();
+    this.m_launcher.stopRollers();
+    this.timer.stop();
+    this.timer.reset();
   }
 
   // Returns true when the command should end.
